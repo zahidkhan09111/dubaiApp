62f678ac912f0c3d1332dac25273995c01173ed6
Zahid Khan <zahidkhan09111@gmail.com>
2021-06-22 10:39:08 +0400

    finishing Sign In Form

---
+++ backend/data.js
@@ -0,0 +1,95 @@
+import bcrypt from 'bcryptjs';
+
+const data = {
+
+  users: [
+    {
+      name: 'Basir',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: false,
+    },
+  ],
+
+    products : [
+        {
+            
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock:10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+           
+            name: 'Adidas Fit Shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 100,
+            countInStock:5,
+            brand: 'Adidas',
+            rating: 3.5,
+            numReviews: 10,
+            description: 'high quality product',
+          },
+          {
+            
+            name: 'Lacoste Free Shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 220,
+            countInStock:0,
+            brand: 'Lacoste',
+            rating: 4.8,
+            numReviews: 17,
+            description: 'high quality product',
+          },
+          {
+          
+            name: 'Nike Slim Pant',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 78,
+            countInStock:10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'high quality product',
+          },
+          {
+           
+            name: 'Puma Slim Pant',
+            category: 'Pants',
+            image: '/images/p5.jpg',
+            price: 65,
+            countInStock:10,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+          },
+          {
+            
+            name: 'Adidas Fit Pant',
+            category: 'Pants',
+            image: '/images/p6.jpg',
+            countInStock:10,
+            price: 139,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 15,
+            description: 'high quality product',
+          },
+        ],
+      };
+      export default data;
---
+++ backend/server.js
@@ -0,0 +1,50 @@
+import express from 'express';
+// import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routers/userRouter.js';
+import productRouter from './routers/productRouter.js';
+
+dotenv.config();
+
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extendedt:true}));
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/dubaiApp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+// Now we are using Mango db so no need of this static data.
+
+// app.get('/api/products/:id', (req, res) => {
+//     const product = data.products.find((X) => X._id=== req.params.id);
+//     if(product) {
+//         res.send(product);
+
+//     }else {
+//         res.status(404).send({message: 'product not found'});
+//     }
+// });
+
+// app.get('/api/products', (req, res) => {
+//     res.send(data.products);
+// })
+
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+
+app.get('/', (req, res)=> {
+    res.send('Server is ready');
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
+  });
+  
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server at http://localhost:${port}`);
+});
