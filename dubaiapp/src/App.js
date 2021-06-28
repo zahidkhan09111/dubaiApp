060e033c8694b6eb3f58d11af241fafc02f3bc11
Zahid Khan <zahidkhan09111@gmail.com>
2021-06-28 17:12:40 +0400

    aa

---
+++ dubaiapp/src/App.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { signout } from './actions/userActions';
+import CartScreen from './screens/CartScreen';
+import HomeScreen from './screens/HomeScreen';
+import OrderScreen from './screens/orderScreen';
+import PaymentMethodScreen from './screens/paymentMethodScreen';
+import PlaceOrderScreen from './screens/placeOrderScreen';
+import ProductScreen from './screens/ProductScreen';
+import RegisterScreen from './screens/registerScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
+import SigninScreen from './screens/SigninScreen';
+
+function App() {
+  const cart = useSelector((state)=> state.cart);
+  const {cartItems} = cart;
+  const userSignin = useSelector((state)=> state.userSignin);
+  const {userInfo} = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
+  return (
+    <BrowserRouter>
+    <div classNameName="grid-container">
+      <header className="row">
+        <div>
+          <Link className="brand" to="/">Tehseen</Link>
+        </div>
+        <div>
+          <Link to="/cart">Cart
+          {cartItems.length>0 && (
+            <span className='badge'>{cartItems.length}</span>
+          )}</Link>
+          {userInfo ? (
+            <div className="dropdown">
+              <Link to="#">
+                {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+              </Link>
+              <ul className="dropdown-content">
+                <li>
+                  <Link to="#signout" onClick={signoutHandler}>
+                    Sign Out
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <Link to="/signin">Sign In</Link>
+          )}
+          
+        </div>
+      </header>
+      <main>
+      <Route path="/product/:id" component={ProductScreen}></Route>
+      <Route path="/cart/:id?" component={CartScreen}></Route>
+      <Route path="/signin" component={SigninScreen}></Route>
+      <Route path="/register" component={RegisterScreen}></Route>
+      <Route path="/shipping" component={ShippingAddressScreen}></Route>
+      <Route path="/payment" component={PaymentMethodScreen}></Route>
+      <Route path="/placeorder" component={PlaceOrderScreen}></Route>
+      <Route path="/order/:id" component={OrderScreen}></Route>
+      <Route path="/" component={HomeScreen} exact></Route>
+        
+      </main>
+      <footer className="row center">All right reserved</footer>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ dubaiapp/src/store.js
@@ -0,0 +1,39 @@
+
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
+import thunk from 'redux-thunk';
+import { cartReducer } from './reducers/cartReducers';
+import { orderCreateReducer, orderDetailsReducer, orderPayReducer } from './reducers/orderReducer';
+import { productdetailsReducer, productListReducer } from "./reducers/productReducers";
+import { userRegisterReducer, userSigninReducer } from './reducers/userSigninReducer';
+
+const initialState ={
+    
+    userSignin: {
+        userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null,
+    },
+    cart: {
+        cartItems: localStorage.getItem('cartItems')
+         ? JSON.parse(localStorage.getItem('cartItems'))
+          : [],
+        shippingAddress: localStorage.getItem('shippingAddress')
+         ? JSON.parse(localStorage.getItem('shippingAddress'))
+         : {},
+         paymentMethod: 'PayPal',
+    },
+};
+const reducer = combineReducers({
+    productList: productListReducer,
+    productDetails: productdetailsReducer,
+    cart: cartReducer,
+    userSignin: userSigninReducer,
+    userRegister: userRegisterReducer,
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
+    orderPay: orderPayReducer,
+});
+
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
+
+export default store;
