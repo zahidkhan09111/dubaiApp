4ff01c7a07a26d449c06df88ef04d83aba4659d8
Zahid Khan <zahidkhan09111@gmail.com>
2021-06-30 09:01:40 +0400

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -40,9 +40,11 @@ app.use('/api/orders', orderRouter);
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
-app.use(express.static(path.join(__dirname, '/dubaiapp/build')));
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/dubaiapp/build/index.html'))
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
